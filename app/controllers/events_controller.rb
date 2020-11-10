a9b14856642373942cb9628bc826d6d5dbbfdb19
William Jackson <williampj@gmail.com>
2020-11-10 09:00:25 -0600

    started EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,26 @@
 class EventsController < ApplicationController 
+  def index 
+
+  end
 end
+
+
+
+
+
+
+
+
+# SCHEMA 
+# create_table "events", force: :cascade do |t|
+#   t.boolean "completed", null: false
+#   t.binary "data", null: false
+#   t.string "inbound_url", null: false
+#   t.string "outbound_url", null: false
+#   t.integer "status_code", null: false
+#   t.datetime "completed_at"
+#   t.bigint "bridge_id", null: false
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.index ["bridge_id"], name: "index_events_on_bridge_id"
+# end
