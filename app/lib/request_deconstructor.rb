81cf04b9e9c388e81ef34f530d9c222d133bfe3f
= <awcrotwell@gmail.com>
2020-11-19 10:01:00 -0500

    Skeleton classes

---
+++ app/lib/request_deconstructor.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative './exceptions'
+
+# TODO: Build
+#
+# Deconstructs the request & response from us & service B. Replaces
+# any headers that contain environment variables with placeholder values
+# to prevent data leakage.
+class RequestDeconstructor
+end
---
+++ app/lib/request_handler.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative './exceptions'
+
+# TODO: Build or delete
+#
+# Thinking some EventWorker functionality will be moved into here.
+# SyntaxParser & RequestDeconstructor would be attributes and aid
+# in setting up & properly saving requests/responses.
+class RequestHandler
+end
