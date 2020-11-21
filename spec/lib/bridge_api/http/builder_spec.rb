481d61aa10039b1363999d70f5921e8010790aa5
= <awcrotwell@gmail.com>
2020-11-21 13:42:28 -0500

    Builder spec

--- spec/lib/bridge_api/http/builder_spec.rb
+++ spec/lib/bridge_api/http/builder_spec.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require_relative './spec_helper'
 
 RSpec.describe BridgeApi::Http::Builder do
   subject do
-    create(:bridge, :with_env)
+    event = create(:event)
+    BridgeApi::Http::Builder.new(
+      MockRequestHandler.new(event),
+      MockPayloadParser.new,
+      MockHeadersParser.new
+    )
   end
 
-  it 'is valid when passed valid info' do
-    expect(subject).to be_valid
+  it 'can generate http objects' do
+    http, req = subject.generate
+
+    expect(http.use_ssl?).to be true
+    expect(http.port).to eq 443
+
+    expect(req['Content-Type']).to eq 'application/json'
+    expect(req['header_1']).to eq 'value_1'
+    expect(req['header_2']).to eq 'value_2'
+    expect(req.uri.to_s).to eq 'https://myfakeoutbound.com'
+    expect(req.body).to eq '{"data":"hello world"}'
   end
 end
--- spec/support/lib/bridge_api/http/mock_request_handler.rb
+++ spec/support/lib/bridge_api/http/mock_request_handler.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class MockRequestHandler
+  attr_reader :event, :bridge
+
+  def initialize(event)
+    @event = event
+    @bridge = event.bridge
+  end
 end
