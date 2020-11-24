0614c74bfe1fbc9ea6dfeba682ad698006e936d0
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-24 00:14:51 -0700

    started implementing action mailer

---
+++ app/mailers/contact_us_mailer.rb
@@ -0,0 +1,9 @@
+class ContactUsMailer < ApplicationMailer
+  default from: 'angelbates5@yahoo.com'
+ 
+  def contact_us
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: 'angelbates5@yahoo.com', subject: 'Welcome to My Awesome Site')
+  end
+end
