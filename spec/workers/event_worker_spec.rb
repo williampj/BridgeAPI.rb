f7fa35722cbba1ddeb5309983687b8712f6717e2
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-25 12:11:17 -0700

    Events controller spec tests creating a job & event worker spec tests processing an event

--- spec/workers/event_worker_spec.rb
+++ spec/workers/event_worker_spec.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative './spec_helper'
+require 'sidekiq/testing'
+
+Sidekiq::Testing.fake!
 
 RSpec.describe EventWorker, type: :worker do
   # it 'Event jobs are enqueued in the scheduled queue' do
@@ -14,7 +17,24 @@ RSpec.describe EventWorker, type: :worker do
   #   end.to change(described_class.jobs, :size).by(1)
   # end
 
-  pending 'can process an event'
+  before do
+    @event = create :event
+    @bridge = @event.bridge
+
+    stub_request(:post, 'https://myfakeoutbound.com/')
+      .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
+      .to_return(status: 200, body: { data: 'stubbed response' }.to_json, headers: {})
+  end
+
+  it 'can process an event' do
+    expect(Event.find(@event.id).completed).to eq false
+
+    EventWorker.new.perform @event.id
+
+    expect(Event.find(@event.id).completed).to eq true
+  end
+
   pending 'can retry 3 times'
+  
   pending 'can clean up when errors are raised'
 end
