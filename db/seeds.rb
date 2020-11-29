a134f2a7769e29f83299076235a0a81330828874
= <awcrotwell@gmail.com>
2020-11-29 13:06:06 -0500

    Remove user 2 from seeds and make bridge 2 belong to the only user

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,6 @@ user = User.find_or_create_by(
   password_digest: '$2a$12$YI.Nl33QVeq3EWruK4QL5.PpZIFYW7wwGNlclCruIXLP4vVFvxtj6', # password
   notifications: false
 )
-user2 = User.find_or_create_by(
-  email: 'tester@bridge.io',
-  password_digest: '$2a$12$YI.Nl33QVeq3EWruK4QL5.PpZIFYW7wwGNlclCruIXLP4vVFvxtj6', # password
-  notifications: false
-)
 
 bridge = Bridge.find_or_create_by(
   user_id: user.id,
@@ -44,7 +39,7 @@ Header.find_or_create_by(key: 'SHOULD_BE_FILTERED', value: '$env.DATABASE_URL', 
 Header.find_or_create_by(key: 'not_filtered', value: 'bridge api', bridge_id: bridge.id)
 
 bridge2 = Bridge.find_or_create_by(
-  user_id: user2.id,
+  user_id: user.id,
   title: 'My Second Bridge',
   inbound_url: 'bridgeapi.com/746353',
   outbound_url: test_url,
@@ -61,11 +56,6 @@ EnvironmentVariable.find_or_create_by(key: 'database', value: 'z9992374623', bri
 EnvironmentVariable.find_or_create_by(key: 'database_password', value: '@@@!++#*!@', bridge_id: bridge2.id)
 Header.find_or_create_by(key: 'X_API_KEY', value: 'returntheslab', bridge_id: bridge2.id)
 Header.find_or_create_by(key: 'Authentication', value: 'Bearer *************', bridge_id: bridge2.id)
-
-# 5.times do
-#   bridge.events.create(completed: false, outbound_url: 'ip.jsontest.com', inbound_url: 'ip.jsontest.com', data: '{"inbound":{"payload":{"FirstName":"Lee","LastName":"Oswald","UserName":"GrassyKnoll","Password":{"nested":"magic bullet"},"Email":"kgb63@yandex.ru"},"dateTime":"2020-11-20T02:02:55.745Z","ip":"::1","contentLength":152},"outbound":[{"request":{"payload":{"FirstName":"Lee","LastName":"Oswald","UserName":"GrassyKnoll","Password":{"nested":"magic bullet"},"Email":"kgb63@yandex.ru"},"dateTime":"2020-11-20T02:02:55.832Z","contentLength":7},"response":{"dateTime":"2020-11-20T02:02:55.882Z","statusCode":"200","message":"OK","size":7,"payload":{"ip":"153.33.111.24"}}}]}', status_code: 300)
-#   bridge2.events.create(completed: false, outbound_url: 'ip.jsontest.com', inbound_url: 'ip.jsontest.com', data: '{"inbound":{"payload":{"FirstName":"Billy","LastName":"Bob","UserName":"Hitman","Password":{"nested":"badaboom"},"Email":"agag@av.ru"},"dateTime":"2020-10-25T02:10:55.745Z","ip":"::2","contentLength":152},"outbound":[{"request":{"payload":{"FirstName":"Lee","LastName":"Oswald","UserName":"GrassyKnoll","Password":{"nested":"magic bullet"},"Email":"kgb63@yandex.ru"},"dateTime":"2020-11-20T02:02:55.832Z","contentLength":7},"response":{"dateTime":"2020-11-20T02:02:55.882Z","statusCode":"200","message":"OK","size":7,"payload":{"ip":"153.33.111.24"}}}]}', status_code: 302)
-# end
 
 %i[success success_with_retries failed aborted aborted_with_retries ongoing].each do |trait|
   FactoryBot.create(:seed_event, trait, bridge_id: bridge.id)
