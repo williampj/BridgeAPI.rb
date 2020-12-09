12e1ad2aa31f3808680147f25d085bd493ed6a20
William Jackson <williampj@gmail.com>
2020-12-08 22:21:24 -0600

    error monitoring

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,28 @@
 
 # Load the Rails application.
 require_relative 'application'
+# For Rollbar monitoring of the Rails boot process
+require_relative 'rollbar'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ENV['ENCRYPTION_SERVICE_SALT'] = Rails.application.credentials[:ENCRYPTION_SERVICE_SALT]
+
+notify = lambda do |e|
+  Rollbar.with_config(use_async: false) do
+    Rollbar.error(e)
+  end
+rescue StandardError
+  Rails.logger.error 'Synchronous Rollbar notification failed.  Sending async to preserve info'
+  Rollbar.error(e)
+end
+
+# rubocop:disable Lint/RescueException
+begin
+  Rails.application.initialize!
+rescue Exception => e
+  notify.call(e)
+  raise
+end
+# rubocop:enable Lint/RescueException
